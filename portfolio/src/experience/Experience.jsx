3ef1ad95ad560920616b12970a16fcafea02f717
James Ducay <jpducay@up.edu.ph>
2025-08-27 10:32:45 +0800

    chore: file setup

--- portfolio/src/experience/Experience.jsx
+++ portfolio/src/experience/Experience.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Experience = () => {
     return (
-        <div className='bg-red-300'>
+        <div className='h-screen bg-red-300'>
             <h1>Experience</h1>
             <ul>
                 <li>Exp1</li>
--- portfolio/src/projects/Projects.jsx
+++ portfolio/src/projects/Projects.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Projects = () => {
     return (
-        <div className='bg-green-300'>
+        <div className='h-screen bg-green-300'>
             <h1>Featured Projects</h1>
             <p>This will be a grid</p>
         </div>
