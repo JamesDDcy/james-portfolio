79655bafcff5392f770a0495670b560f5c7c11c8
James Ducay <jpducay@up.edu.ph>
2025-08-31 22:03:06 +0800

    feat: progress-bar

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,21 +1,40 @@
+
 import About from './about/About';
 import './App.css'
 import Experience from './experience/Experience';
 import Home from './home/Home';
 import Navbar from './navbar/Navbar';
 import Projects from './projects/Projects';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [scrollPercent, setScrollPercent] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.scrollY;
+      const docHeight = document.documentElement.scrollHeight - window.innerHeight;
+      const percent = docHeight > 0 ? (scrollTop / docHeight) * 100 : 0;
+      setScrollPercent(percent);
+    };
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // set initial value
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
     <div className='relative'>
       <div className='fuzzy-overlay'></div>
+      <div className="fixed w-full h-0.5 z-50">
+        <div className="h-full bg-amber-600" style={{ width: `${scrollPercent}%` }}></div>
+      </div>
       <Navbar></Navbar>
       <Home></Home>
       <About></About>
       <Experience></Experience>
       <Projects></Projects>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
