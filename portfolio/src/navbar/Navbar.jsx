cb701df589f78e3f2338cf85f7f8d3128ade2887
James Ducay <jpducay@up.edu.ph>
2025-09-09 15:22:55 +0800

    feat: navbar animation

--- portfolio/src/navbar/Navbar.jsx
+++ portfolio/src/navbar/Navbar.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import './Navbar.css'
+import RevealAnimation from '../components/RevealAnimation';
 
 
 const Navbar = () => {
@@ -18,8 +19,12 @@ const Navbar = () => {
     return (
         <div className='w-full fixed top-10 z-50 flex justify-end text-sm'>
             <div className={`flex justify-start w-1/5 py-5 px-15 gap-16 rounded-s-lg transition-all duration-2000 ${blur ? 'backdrop-blur-xs' : ''}`}>
-                <a href="#about" className='line-link'>About</a>
-                <a href="#projects" className='line-link'>Projects</a>
+                <RevealAnimation delay={0.5} y={-75}>
+                    <a href="#about" className='line-link'>About</a>
+                </RevealAnimation>
+                <RevealAnimation delay={1} y={-75}>
+                    <a href="#projects" className='line-link'>Projects</a>
+                </RevealAnimation>
             </div>
         </div>
     );
