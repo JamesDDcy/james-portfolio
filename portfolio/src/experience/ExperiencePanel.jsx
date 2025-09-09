0d1ee98f3d560a9668700b40ba724a690199ae05
James Ducay <jpducay@up.edu.ph>
2025-09-09 17:13:49 +0800

    feat: responsive experience panel section

--- portfolio/src/experience/ExperiencePanel.jsx
+++ portfolio/src/experience/ExperiencePanel.jsx
@@ -14,7 +14,7 @@ const ExperiencePanel = (prop) => {
     }, []);
 
     return (
-        <div className='w-3/5 border border-white/10 bg-white/10 shadow-2xl backdrop-blur-md rounded-2xl z-50 p-3'>
+        <div className='w-4/5 xl:w-3/5 border border-white/10 bg-white/10 shadow-2xl backdrop-blur-md rounded-2xl z-50 p-3'>
             <div className='grid grid-cols-4'>
                 <div className='col-span-1 flex justify-center items-center'>
                     {logo && <img className='w-1/2 object-contain' src={logo} alt='logo' />}
